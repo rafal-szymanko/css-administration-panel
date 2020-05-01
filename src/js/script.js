c79608c4c048557d28b277edfd429565f1230d1e
Rafal Szymanko <szymanko@gmail.com>
2020-05-01 09:37:23 +0200

    Add styles for links section

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,15 @@
-// const sidebar = document.querySelector('.fa-bars');
-// const navigation = document.querySelector('.navigation');
-// const mainSide = document.querySelector('main');
+const sideBarHandler = document.querySelector('.fa-bars');
+const navigation = document.querySelector('.menu');
+const mainSide = document.querySelector('main');
 // const messenger = document.querySelector('.manager');
 
-// sidebar.addEventListener('click', function(){
-// //   navigation.classList.toggle('active');
-// //   mainSide.classList.toggle('active');
-// //   messenger.classList.toggle('active');
-//   console.log('object');
+sideBarHandler.addEventListener('click', function(){
+  navigation.classList.toggle('menu--show');
+  mainSide.classList.toggle('main--show');
+//   messenger.classList.toggle('active');
 
-// });
+
+});
 
 
 //Input dates
@@ -34,3 +34,15 @@ const datepickers = document.querySelectorAll('.section__datepicker');
 for (let datepicker of datepickers) {
   datepicker.value = today;
 }
+
+//Visible menu
+
+// function toggleMenu(visible) {
+//   document.querySelector('.menu').classList.toggle('menu--show', visible);
+//   console.log('object');
+// }
+
+// document.querySelector('.fa-bars').addEventListener('click', function(e) {
+//   e.preventDefault();
+//   toggleMenu();
+// });
