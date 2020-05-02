f0d8d288b1dfd55efec665a32f9fe90b7f0217ab
Rafal Szymanko <szymanko@gmail.com>
2020-05-02 11:11:26 +0200

    Add chart.js library

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 const sideBarHandler = document.querySelector('.fa-bars');
 const navigation = document.querySelector('.menu');
 const linkNames = document.querySelectorAll('.menu__text');
@@ -50,14 +52,50 @@ for (let datepicker of datepickers) {
   datepicker.value = today;
 }
 
-//Visible menu
+// Chart
 
-// function toggleMenu(visible) {
-//   document.querySelector('.menu').classList.toggle('menu--show', visible);
-//   console.log('object');
-// }
+const ctx = document.querySelector('.section__chart');
 
-// document.querySelector('.fa-bars').addEventListener('click', function(e) {
-//   e.preventDefault();
-//   toggleMenu();
-// });
+const data = {
+  labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+  datasets: [
+    {
+      label: 'Signups',
+      backgroundColor: '#80BFCA',
+      data: [8, 414, 500, 303, 159, 164, 335, 310, 584, 580]
+    },
+    {
+      label: 'FTD',
+      backgroundColor: '#FFA200',
+      data: [101, 197, 553, 188, 178, 402, 381, 568, 256, 222]
+    },
+    {
+      label: 'Earned',
+      backgroundColor: '#59B66D',
+      data: [388, 150, 495, 16, 155, 432, 395, 259, 82, 237]
+    }
+  ]
+};
+
+const myBarChart = new Chart(ctx, {
+  type: 'bar',
+  data: data,
+  options: {
+    barValueSpacing: 20,
+    scales: {
+      yAxes: [{
+        ticks: {
+          min: 0,
+        }
+      }]
+    },
+    
+    legend: {
+      display: true,
+      labels: {
+        fontColor: '#333',
+        usePointStyle:true,
+      }
+    }
+  }
+});
