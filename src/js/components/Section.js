b04edc144d3dba0303d2e39e3947eb496d1a7ad6
Rafal Szymanko <szymanko@gmail.com>
2020-05-11 21:29:53 +0200

    Add style for clicked list item

--- src/js/components/Section.js
+++ src/js/components/Section.js
@@ -20,16 +20,18 @@ class Section{
       menuLink.addEventListener('click', function(event){
         event.preventDefault();
         const clickedElement = this;
-
         thisSection.activeLinks = document.querySelectorAll('.menu__item--active');
 
 
         for (let activeLink of thisSection.activeLinks) {
           activeLink.classList.remove('menu__item--active');
+          activeLink.style.background = '#191919';
           thisSection.navigationMobile.classList.remove('menu__list--show');
         }
 
         clickedElement.classList.add('menu__item--active');
+        clickedElement.style.background = '#282828';
+
         const sectionSelector = clickedElement.querySelector('.menu__link').getAttribute('href').slice(1);
         thisSection.activeSections = document.querySelectorAll('.section--active');
 
